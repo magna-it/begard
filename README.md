a8b6cc9f88c78ea174835f2c165136278ae95cde
mohsen shafiei <mohsen.jch@gmail.com>
2015-06-04 17:21:12 +0430

    Initialize directory review

---
+++ README.md
@@ -0,0 +1,7 @@
+# Begard
+
+**Begard is still in development**
+
+## What is Begard?
+
+Begard is a light, simple and powerful file manager for web clients.
--- src/js/begard.js
+++ src/js/begard.js
@@ -1,30 +1,132 @@
 (function() {
     var begardFactory;
 
-    begardFactory = function($, _) {
+    begardFactory = function($) {
+
+        /**
+         * Keep private methods and variables
+         */
+        var b = {
+
+            /**
+             * Default options
+             */
+            options: {
+                remote: '',
+                method: 'POST',
+                templates: {
+                    directory: '<li><a href="#" class="begard-directory" data-path="{data-path}">{folder-name}</a></li>'
+                }
+            },
+
+            /**
+             * All data about files and folders
+             */
+            data: [],
+
+            /**
+             * Initialize begard
+             */
+            init: function() {
+                b.handleEvents();
+                b.getData('/');
+            },
+
+            /**
+             * Handle events
+             */
+            handleEvents: function() {
+                $(document).on('click', '.begard-directory', function(e) { var self = $(this); b.openFolder(e, self); });
+            },
+
+            /**
+             * Get data for specific path
+             */
+            getData: function(path) {
+                $.ajax({
+                    url: b.options.remote,
+                    data: {
+                        path: path
+                    },
+                    method: b.options.method,
+                    dataType: "json"
+                }).done(function(data) {
+                    var path = data.path;
+                    delete data.path;
+
+                    b.data[path] = data;
+                    b.refreshView(path);
+                });
+            },
+
+            rename: function() {
+
+            },
+
+            delete: function() {
+
+            },
+
+            refreshView: function(path) {
+                b.refreshFolders(path);
+            },
+
+            openFolder: function(e, self) {
+                var path = self.attr('data-path');
+
+                b.getData(path);
+            },
+
+            /**
+             * Refresh directory list by given path
+             * @param {string} path
+             */
+            refreshFolders: function(path) {
+                $('#begard-directories ul li').remove();
+                $.each(b.data[path].directories, function(index, folderName) {
+
+                    //Do not add extra slash when path is root
+                    if (path !== '/')   basePath = path + '/' + folderName;
+                    else                basePath = path + folderName;
+
+                    //Add directory to html
+                    var template = b.options.templates.directory;
+                    template = template.replace('{data-path}', basePath);
+                    template = template.replace('{folder-name}', folderName);
+                    $('#begard-directories ul').append(template);
+                });
+            },
+        };
+
         return begard = {
+
             /**
              * Set configuration
              * @param {object} options
              */
             config: function(options) {
-
+                $.extend(true, b.options, options);
             },
 
             /**
              * Start begard normally
-             * @param {string} element - Selector of begard DOM element
+             * @param {string} template - Selector of underscore template
+             * @param {string} appendTo - Selector of element
              */
-            standolne: function(element) {
+            standalone: function(begardTemplate, appendTo) {
+                //Initialize
+                b.init();
+
 
             },
 
             /**
              * Start begard as a modal
-             * @param {string} element - Selector of begard DOM element
+             * @param {string} template - Selector of underscore template
              */
-            modal: function(element) {
-
+            modal: function(template) {
+                //Initialize
+                b.init();
             },
 
             /**
@@ -38,10 +140,10 @@
 
 
     if (typeof define === 'function' && define.amd) {
-        define(['jquery', 'underscore'], begardFactory);
+        define(['jquery'], begardFactory);
     } else if (typeof exports === 'object') {
-        module.exports = begardFactory(require('jquery', 'underscore'));
+        module.exports = begardFactory(require('jquery'));
     } else {
-        window.begard = begardFactory(window.jQuery, window._);
+        window.begard = begardFactory(window.jQuery);
     }
 }).call(this);
